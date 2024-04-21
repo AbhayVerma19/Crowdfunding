8e6c0f6e8892c667592be2986d334ff453af6ae8
AbhayVerma19 <abhver192002@gmail.com>
2024-04-21 14:27:25 +0530

    Styling  Header 2:57

---
+++ minor2/components/layout/components/HeaderRight.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import { App } from "../Layout";
+import { useContext } from "react";
+
+const HeaderRight = () => {
+  const ThemeToggler = useContext(App);
+
+  return (
+    <HeaderRightWrapper>
+      <ThemeToggle onClick={ThemeToggler.changeTheme}>
+        {ThemeToggler.theme === "light" ? <DarkModeIcon /> : <LightModeIcon />}
+      </ThemeToggle>
+    </HeaderRightWrapper>
+  );
+};
+
+const HeaderRightWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 16px;
+  height: 50%;
+`;
+const ThemeToggle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.bgDiv};
+  height: 100%;
+  padding: 5px;
+  width: 45px;
+  border-radius: 12px;
+  cursor: pointer;
+`;
+
+export default HeaderRight;
